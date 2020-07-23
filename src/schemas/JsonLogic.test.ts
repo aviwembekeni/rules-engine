5b6564d6ffad6e5497d3bc0547c87d850b26bdf9
Kenny Inggs <kenny.inggs@gmail.com>
2020-07-23 16:38:19 +0200

    Whitespace

--- src/schemas/JsonLogic.test.ts
+++ src/schemas/JsonLogic.test.ts
@@ -12,24 +12,28 @@ describe('JsonLogic Tests', (): void => {
     const result = jsonLogic.apply(rule, data);
     expect(result).toBe(true);
   });
+
   it('1: Compare value > threshold', async (): Promise<void> => {
     const data = testScenarios[1];
     const rule = { '>': [{ var: 'value' }, { var: 'threshold' }] };
     const result = jsonLogic.apply(rule, data);
     expect(result).toBe(true);
   });
+
   it('2: Simple nesting selection value > threshold', async (): Promise<void> => {
     const data = testScenarios[2];
     const rule = { '>': [{ var: 'portfolio.value' }, { var: 'portfolio.threshold' }] };
     const result = jsonLogic.apply(rule, data);
     expect(result).toBe(true);
   });
+
   it('3: Check all items in array value > threshold', async (): Promise<void> => {
     const data = testScenarios[3];
     const rule = { map: [{ var: 'portfolios' }, { '>': [{ var: 'value' }, { var: 'threshold' }] }] };
     const result = jsonLogic.apply(rule, data);
     expect(result).toStrictEqual([true, true, false]);
   });
+
   it('4: Array Filter matching (id=1) value > threshold', async (): Promise<void> => {
     // I can't find a way to achieve this in a single rule. A workaround is to manually chain filters into maps, but it feels yucky
     const data = testScenarios[4];
@@ -43,6 +47,7 @@ describe('JsonLogic Tests', (): void => {
     const result = jsonLogic.apply(rule, filterResult);
     expect(result).toStrictEqual([true]);
   });
+
   it('5: Array-in-Array with filters', async (): Promise<void> => {
     // Similar to 4, but worse. I think I need to chain-filter :(
     const data = testScenarios[5];
